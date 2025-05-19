49a1984805b0be0489e62fde38496a6b0eee8b2c
Since2024 <hasonmagar@gamil.com>
2025-05-19 23:04:12 +0545

    Initial commit

---
+++ day1.py
@@ -0,0 +1,14 @@
+# ask user what is your name?
+#name = input("What is your name?")
+#"""
+#Another technique to comment
+
+#"""
+#Address= input("Where do you live?")
+#print("HEllo,"+name +". So,you are from "+ Address)
+
+
+import calendar
+yy = 2025 
+mm = 12
+print(calendar.month(yy,mm))
---
+++ day2.py
@@ -0,0 +1,19 @@
+# import threading 
+# def add_numbers(x, y):
+#    print(x + y)
+# t = threading.Thread(target=add_numbers, args=(5, 7))
+# t.start()
+# t.join()
+
+
+# import time
+# from plyer import notification
+
+# if __name__ == "__main__":
+#     while True:
+#         notification.notify(
+#             title="ALERT!!",
+#             message="Take a break!",
+#             timeout=10
+#         )
+#         time.sleep(3600)
