7161cc60d97ae509697ebcad29d0ff2dfbbb7d66
Since2024 <hasonmagar@gmail.com>
2025-07-27 10:46:02 +0545

    Update

---
+++ Basics.py
@@ -0,0 +1,11 @@
+message = "Don't move"
+#print(pin)
+
+
+#Function are code blocks that can be reused that you can call or to bring in action
+def pin(): #def means define a function, (invoke =action) : means the start of the function
+    print(message)
+
+pin() #IS INVOKED AT END TO RUN CODE
+
+
