5e9c13a474a38074eedb6088a56e27dee76841af
Since2024 <hasonmagar@gmail.com>
2025-07-28 11:48:36 +0545

    intial commit

--- Basics.py
+++ Basics.py
@@ -1,11 +1,26 @@
-message = "Don't move"
+#message = "Don't move"
 #print(pin)
 
 
 #Function are code blocks that can be reused that you can call or to bring in action
-def pin(): #def means define a function, (invoke =action) : means the start of the function
-    print(message)
+#def pin(): #def means define a function, (invoke =action) : means the start of the function
+ #   print(message) message which is variable is now argument
+#pin() #IS INVOKED AT END TO RUN CODE
 
-pin() #IS INVOKED AT END TO RUN CODE
+
+# string = "hasan gaha"
+# print(string[4])
+#  indexing starts at 0, this start from front is positive integers.
+#to access from last string charcter we can use -1 to -n
+
+
+#to get total number of charcaters in string we use builtin function len(variables name)
+# air = "hawa"
+# print(len(air))
+
+#built in function type(V.N) which returns data type of variable
+# air = "hawa"
+# water = 100
+# print(type(air), type(water))
 
 
