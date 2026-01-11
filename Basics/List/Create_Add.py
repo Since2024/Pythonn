63ae22e6495791bb202ab59f4d8cca4ce26401c4
Since2024 <hasonmagar@gmail.com>
2026-01-11 10:39:06 +0545

    list pract

---
+++ Basics/List/Create_Add.py
@@ -0,0 +1,24 @@
+#empty list
+my_list = []
+
+#integer list
+my_list = [1, 2, 3]
+
+#list with mixed data
+my_list = [1, "apple", 2.9, 13]
+
+#nested list
+my_list = ["banana", [2,4,5],['s']]
+
+#append() method to add one item in list
+#extend() method to add several items in list
+odd = [1,3,5]
+odd.append(7)
+print(odd)
+
+odd.extend([9, 13, 11])
+print(odd)
+#we can also use + operator to combine two list which is called concatenation. 
+
+
+
