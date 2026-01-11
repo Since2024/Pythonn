5262e7f89d00f6174bd4c23fab6db8472bfe503c
Since2024 <hasonmagar@gmail.com>
2026-01-11 10:48:05 +0545

    delete or removing in list or the list

---
+++ Basics/List/Delete.py
@@ -0,0 +1,10 @@
+my_list = ['a', 'c', 'd', 'f', 'tap', 'e', 'g']
+del my_list[2] #delete one elements
+
+#delete multiple elements 
+del my_list[1:3]
+
+
+#delete entire list
+del my_list
+print(my_list)
