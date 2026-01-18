2766e531092dca9fa2ee24f397488187b3837bf3
Since2024 <hasonmagar@gmail.com>
2026-01-18 08:52:29 +0545

    another way to reverse that read list backward

--- Basics/Array/reverse.py
+++ Basics/Array/reverse.py
@@ -4,5 +4,9 @@ arr = [3, 2, 4, 5, 1]
 # print(reverse_arr)
 
 #reverse() method. It modifies the existing array in memory without creating new one.
-arr.reverse()
-print(arr)
+# arr.reverse()
+# print(arr)
+
+#reversed() function (the iterator way) that reads list backward
+for x in reversed(arr):
+    print(x, end=" ")
