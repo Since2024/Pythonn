1423581ee92f8303257f613f32e69f61e1456d46
Since2024 <hasonmagar@gmail.com>
2026-01-18 08:46:34 +0545

    another method using reverse() method

--- Basics/Array/reverse.py
+++ Basics/Array/reverse.py
@@ -1,3 +1,8 @@
+#slicing way which uses [start:stop:step] syntax setting step to -1 tell walk backward
 arr = [3, 2, 4, 5, 1]
-reverse_arr = arr[::-1]
-print(reverse_arr)
+# reverse_arr = arr[::-1]
+# print(reverse_arr)
+
+#reverse() method. It modifies the existing array in memory without creating new one.
+arr.reverse()
+print(arr)
