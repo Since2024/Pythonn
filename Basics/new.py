848284a8f6cae53711af825f2da3586664b099ae
Since2024 <hasonmagar@gmail.com>
2026-02-05 12:44:24 +0545

    whatever

---
+++ Basics/new.py
@@ -0,0 +1,19 @@
+# commet = "katprikat"
+# print(commet.upper())
+# print(len(commet))
+# print(commet[-9])
+# print(commet.strip())
+# print(commet.find("r"))
+# print(commet.replace("t","o"))
+# print("kat" in commet)
+# print("no" not in commet)
+
+# print(10 // 3)
+# import math
+# print(round(2.9))
+# print(abs(-2.9))
+
+# print(math.ceil(2.2))
+x = input("x: ")
+y = int(x) + 1
+print(y)
