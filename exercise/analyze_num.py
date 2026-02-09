4f52b4c20a2f8ec1303f4b08120feae036fea785
Since2024 <hasonmagar@gmail.com>
2026-02-09 21:23:43 +0545

    finding odd/even with range

---
+++ exercise/analyze_num.py
@@ -0,0 +1,20 @@
+def analyze_numbers(n):
+    even_count = 0
+    odd_count = 0
+
+    for num in range(1, n + 1):
+        if num % 2 == 0:
+            print(num, "is Even")
+            even_count += 1
+        else:
+            print(num, "is Odd")
+            odd_count += 1
+
+    print("\nSummary:")
+    print("Total Even numbers:", even_count)
+    print("Total Odd numbers:", odd_count)
+
+
+# Program starts here
+limit = int(input("Enter a number: "))
+analyze_numbers(limit)
