bac2041e8103f06280fd10ad4be8190bf9106006
Since2024 <hasonmagar@gmail.com>
2026-05-29 20:38:36 +0545

    learnig indexing for data parsing

---
+++ exercise/slicing.py
@@ -0,0 +1,11 @@
+//Display only those characters which are present at an even index number in given string.
+word = "pynative"
+print("Original String is ", word)
+
+# Method: Using list slicing
+# format: [start:stop:step]
+even_chars = word[0::2]
+
+print("Printing only even index chars")
+for char in even_chars:
+    print(char)
